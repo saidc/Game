0ed28a7f858345dbbb7c99607181275e1296a4e7
SaidC <saidjoc@gmail.com>
2020-10-08 11:20:05 -0500

    beging a class to handle Mouse Listener

---
+++ Game/src/main/java/Jade/MouseListener.java
@@ -0,0 +1,26 @@
+
+package Jade;
+
+public class MouseListener {
+    private static MouseListener instance;
+    private double scrollX,scrollY ;
+    private double xPos, yPos, lastX,lastY;
+    private boolean mouseButtonPressed[] = new boolean[3];
+    private boolean isDragging ;
+    
+    private MouseListener(){
+        this.scrollX = 0.0;
+        this.scrollY = 0.0;
+        this.xPos    = 0.0;
+        this.yPos    = 0.0;
+        this.lastX   = 0.0;
+        this.lastY   = 0.0;
+    }
+    
+    public static MouseListener get(){
+        if(MouseListener.instance == null){
+            MouseListener.instance = new MouseListener();
+        }
+        return MouseListener.instance;
+    }
+}
