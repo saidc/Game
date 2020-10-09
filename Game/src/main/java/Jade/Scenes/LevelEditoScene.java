08716ba672b29cc522804355312b60ddce185bed
SaidC <saidjoc@gmail.com>
2020-10-09 00:23:40 -0500

    adding a explample of using Time changing a Scene when a button is pressed but it have to wait 2 seconds

--- Game/src/main/java/Jade/Scenes/LevelEditoScene.java
+++ Game/src/main/java/Jade/Scenes/LevelEditoScene.java
@@ -7,20 +7,28 @@ import Jade.EventListener.keysListener;
 import org.lwjgl.glfw.GLFW;
 
 public class LevelEditoScene extends Scene {
+    private float   timeToChangeScreen ;
+    private boolean changeScreen ;
     public LevelEditoScene(){
         System.out.println("you are in LevelEditorScene");
+        timeToChangeScreen = 2.0f;
+        changeScreen = false;
     }
-
+    
     @Override
     public void update(float dt) {
         if(keysListener.isKeyPress(GLFW.GLFW_KEY_SPACE)){
             System.out.println(" Space key is pressed in LevelEditorScene");
         }
-        if(keysListener.isKeyPress(GLFW.GLFW_KEY_A)){
+        
+        if(!this.changeScreen && keysListener.isKeyPress(GLFW.GLFW_KEY_A)){
             System.out.println(" A key is pressed");
-            Window.changeScene(1);
+            this.changeScreen = true;
         }
         
+        if(changeScreen && (timeToChangeScreen -= dt) < 0){
+            this.changeScreen = false;
+            Window.changeScene(1);
+        }
     }
-    
 }
